3f91c188bd44b6c4301c35165652a7c21d74285e
筱筱 <xiaoxiao@xiaoxiaodeMacBook-Pro.local>
2018-07-16 16:38:04 +0800

    修改数据获取方式

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## 安装
 
-```bash
+``` bash
 # install dependencies
 npm install
 
@@ -19,10 +19,8 @@ npm run build --report
 ```
 
 #### 使用指南
-
 ##### 使用默认数据,
-
-可以获取通过 onchange 方法，获取更改后的数据。
+可以获取通过onchange方法，获取更改后的数据。
 
 ```
 <template>
@@ -44,95 +42,81 @@ export default {
 ```
 
 ##### 一次性加载所有数据
-
-注意：mode = 1,可以使用 v-model 获取修改后的数据；
-
+注意：mode = 1,可以使用v-model获取修改后的数据；
 ```
 <template>
     <area-picker
-	    	@onload = "loadData"
-	    	@onchange ="getChoosed"
-	    	mode="1"
-	    	:initlist="initlist"
-	    	:initShowList="initShowList",
-	    	v-model="initShowList"
-	    	label="省市区">
-    </area-picker>
-</template>
-
-<script>
-export default {
-	data() {
-		return {
-		    initShowList: [
-				{ id: "610000", area_name: "陕西省" },
-				{ id: "610700", area_name: "汉中市" },
-				{ id: "610724", area_name: "西乡县" }
-			],
-			initList:[]
-		};
-	},
-	methods: {
-		getChoosed(val) {
-			console.log(val);
-		},
-		loadData(opts){
-		    //数据请求成功后
-		    this.initList = result;
-		}
-	}
-};
-</script>
-```
-
-##### 分块加载数据
-
-注意：设置 mode=2;
-
-```
-<template>
-    <area-picker
-	    	@onload = "loadData"
-	    	@onchange ="getChoosed"
-	    	:initlist="initlist"
-	    	mode="2"
-	    	label="地址">
+	    	:data = "getAreas"
+	    	:mode=1
+	    	child="child",
+	    	pid="parent_id",
+	    	orgdata={true}
+	    	@onchange ="getChoosed">
     	</area-picker>
 </template>
 
 <script>
 export default {
 	data() {
-		return {
-			initList:[]
-		};
+		return {};
 	},
 	methods: {
 		getChoosed(val) {
 			console.log(val);
 		},
-		loadData(opts){
-		    //数据请求成功后
-		    this.initList = result;
+		async getAreas(){
+		    //返回一个promise
+		    return Promise
 		}
 	}
 };
 </script>
 ```
 
-##### 兼容 form-item
 
-注意：vbeauty = true; prop 设置成关联字段名称，v-model 设置成关联字段，具体用法和 form-item 用法一样。
+##### 分块加载数据
+注意：设置mode=2;
+```
+<template>
+    <area-picker
+	    	:data = "getAreas"
+	    	:mode=2
+	    	@onchange ="getChoosed">
+    	</area-picker>
+</template>
 
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		getChoosed(val) {
+			console.log(val);
+		},
+		async getAreas(){
+		    //返回一个promise
+		    return Promise
+		}
+	}
+};
+</script>
+```
+
+##### 兼容form-item 
+注意：vbeauty = true; prop设置成关联字段名称，v-model设置成关联字段，具体用法和form-item用法一样。
 ```
 <template>
     <v-form :model="customForm" :rules="customRules" ref='customRuleForm'>
         <area-picker
-	    	@onchange ="getChoosed"
+	    	:data = "getAreas"
+	    	:initShowList="customForm.address"
 	    	:vbeauty=true
+	    	:mode=2
 	    	prop="address"
 	    	v-model="customForm.address"
-    	></area-picker>
+	    	@onchange ="getChoosed">
+    	</area-picker>
     </v-form>
 </template>
 
@@ -142,7 +126,11 @@ export default {
 	    //可参照form-item设置自定义验证规则
 		return {
 		    customForm: {
-				address: "",
+				[
+					{ code: "360000", name: "江西省" },
+					{ code: "360100", name: "南昌市" },
+					{ code: "360101", name: "直辖区" }
+				]
 			},
 			customRules: {
 				address: [
@@ -157,33 +145,52 @@ export default {
 	methods: {
 		getChoosed(val) {
 			console.log(val);
+		},
+		async getAreas(){
+		    //返回一个promise
+		    return Promise
 		}
 	}
 };
 </script>
 ```
 
-#### API
 
+
+#### API
 ##### area-picker props
 
-| 参数         | 说明                                                    | 类型    | 默认值    |
-| ------------ | ------------------------------------------------------- | ------- | --------- |
-| mode         | 0:使用默认数据；1:一次加载所有数据；2:分块加载数据      | Number  | 0         |
-| clue         | 显示值                                                  | String  | id        |
-| name         | 对象显示名                                              | String  | area_name |
-| label        | 标签名                                                  | String  | "地址"    |
-| initlist     | 数据：（分片/一次）请求数据                             | Array   | -         |
-| initShowList | 设置默认值                                              | Array   | -         |
-| pid          | 当一次性加载数据时，重组数据时的关联字段                | String  | parent_id |
-| child        | 数据层级名称定义                                        | String  | child     |
-| vbeauty      | 是否使用 form-item,如果为 true,则可以设置表单验证内容， | Boolean | false     |
-| v-model      | 双向绑定数据【兼容 form-item 时需要】，                 | -       | -         |
-| prop         | vbeauty = true 时，对应表单中的字段名称                 | String  | -         |
+参数  | 说明 | 类型 | 默认值
+---|---|---|---|
+ mode | 0:使用默认数据；1:一次加载所有数据；2:分块加载数据 | Number | 0
+ clue | 选项的id | String | id
+ name | 选项的名称 | String | area_name
+ label | 标签名 | String | "地址"
+ initShowList | 设置默认值 | Array | -
+ pid | 当一次性加载数据时，重组数据时的关联字段 | String | parent_id
+ child | 数据层级名称 | String | child
+ orgdata | 是否使用默认重组数据方法 | Boolean | false
+ vbeauty | 是否使用form-item,如果为true,则可以设置表单验证内容 | Boolean | false
+ v-model| 双向绑定数据【兼容form-item时需要】| - | -
+ prop | form-item 的prop属性，配合vbeauty=true使用 | String | -
+ 
+ 
+ 
+ 
+#####  area-picker Method
 
-##### area-picker Method
+事件  | 说明 | 参数 
+---|---|---|---|
+onchange | 选择的值发生变化的时候触发，默认返回选中区域数组 | value
 
-| 事件     | 说明                                             | 参数  |
-| -------- | ------------------------------------------------ | ----- |
-| onload   | 用户自定义加载数据                               | -     |
-| onchange | 选择的值发生变化的时候触发，默认返回选中区域数组 | value |
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
--- src/components/area-picker/area-picker.vue
+++ src/components/area-picker/area-picker.vue
@@ -2,7 +2,7 @@
 	 	<div class="ant-address">
 	 		<v-form-item label="地址" required  :prop="prop" v-if="vbeauty" >
 		 		<div class="ant-address__text" @click="toggleDropdown" >
-		 			<v-input type="text" placeholder="请选择" size="middle" readonly v-bind="$attrs" :value="addressValue">
+		 			<v-input type="text" placeholder="请选择" size="middle" readonly v-bind="$attrs" v-model="finish">
 		 			</v-input>
 					<span class="ant-address__clear" v-show="finish.length >= 2" @click.stop="clear"></span>
 		 		</div>
@@ -10,7 +10,7 @@
 	 		<div class="ant-address__label" v-else>
 	 			<div class="ant-address__label_inner">{{label}}：</div>
 		 		<div class="ant-address__text ant-address__border ant-address__pad" @click="toggleDropdown" >
-		 			<span>{{addressValue}}</span>
+		 			<span>{{finish}}</span>
 		 			<span class="ant-address__clear" v-show="finish.length >= 2" @click.stop="clear"></span>
 		 		</div>
 	 		</div>
@@ -20,46 +20,38 @@
 	                <li v-for="(item, index) in current" @click="showCurrent(item, index)" :class=" index == currentIndex ? 'actived' : '' ">{{item[name] ? item[name]: item.name}}</li>
 	            </ul>
 	            <ul class="ant-address__select-content">
-	            	<li class="loading" v-if="showList.length ==0 ">加载中。。。</li>
+	            	<li class="loading" v-if="showList.length == 0 ">加载中。。。</li>
 	                <li v-for="item in showList" @click="getNext(item)" :class="current[currentIndex][clue] == item[clue] ? 'actived' : '' ">{{item[name] ? item[name] : item}}</li>
 	            </ul>
 	        </div>
 	 	</div>
 </template>
 
-
-
-
 <script type="text/ecmascript-6">
-//不管是一次性加载所有的数据还是分块加载，都需要对数据进行处理。
-
-//如果一次性加载所有的数据，需要用户定义二级名称
-import data from "./data.js";
+import data from './data.js'
 export default {
-	props: {
-		initShowList: {
-			type: Array,
-			default: () => []
-		},
-		clue: {
+    props: {
+        initShowList: {
+            type: Array,
+            default: () => []
+        },
+        // 数据加载函数，返回值必须是Promise;
+        // 默认情况下必须传递data参数；如果使用本地数据渲染表格，业务代码中将获取本地数据包装为Promise即可。
+        data: {
+            type: Function,
+            default:()=>{}
+        },
+        mode:{
+        	type:Number,
+        	default:0 //0:使用默认数据，1:一次性加载所有数据，2:使用分块加载
+        },
+        clue: {
 			type: String,
 			default: "id"
-		},
-		pid: {
-			type: String,
-			default: "parent_id"
-		},
-		initlist: {
-			type: Array,
-			default: () => []
 		},
 		name: {
 			type: String,
 			default: "area_name"
-		},
-		mode: {
-			type: String,
-			default: "0"
 		},
 		child: {
 			type: String,
@@ -73,162 +65,67 @@ export default {
 			type: Boolean,
 			default: false
 		},
-		prop: String
-	},
-	data() {
-		return {
-			showPanel: false,
-			list: {},
-			showList: [],
-			currentIndex: 0, // 1,2,3
-			current: [
-				{
-					name: "请选择"
-				}
-			],
-			finish: []
-		};
-	},
-	computed: {
-		addressValue() {
-			return this.finish
-				.map(item => (item[this.name] ? item[this.name] : ""))
-				.join(" ");
-		}
-	},
-	created() {
-		//触发用户定义的promise事件；
-		//this.showList = await this.getProvince();
-		// 初始化回显
-
-		if (Array.isArray(this.initShowList) && this.initShowList.length > 0) {
-			this.current = [].concat(this.initShowList);
-			this.finish = [].concat(
-				this.current.map(item => {
-					return {
-						[this.clue]: item[this.clue],
-						[this.name]: item[this.name]
-					};
-				})
-			);
-			if (this.mode === "2") {
-				var ln = this.current.length;
-				switch (ln) {
-					case 1:
-						this.$emit("onload", {
-							level: 1
-						});
-						this.currentIndex = 0;
-						break;
-					case 2:
-						this.$emit("onload", {
-							level: 2,
-							level_code: this.current[0][this.clue]
-						});
-						this.currentIndex = 1;
-						break;
-					case 3:
-						this.$emit("onload", {
-							level: 3,
-							level_code: this.current[1][this.clue]
-						});
-						this.currentIndex = 2;
-						break;
-				}
-			} else if (this.mode === "1") {
-				this.showCurrent(this.current[0], 0);
-			}
-		} else {
-			if (this.mode === "1" || this.mode === "2") {
-				this.showCurrent(this.current[0], 0);
-			}
-		}
-
-		if (this.mode === "0") {
-			this.list = data;
-			this.showList = this.list;
-			this.handlerRecover();
-			return;
-		}
-	},
-	watch: {
-		finish(val) {
-			this.addressValue;
-			this.$emit("input", val);
+		prop: String,
+		orgdata:{ //当一次性加载数据时，是否需要重组数据，
+			type:Boolean,
+			default:true 
 		},
-		initlist: function() {
-			this.showList = this.initlist;
-			//once load data
-			if (this.mode === "1") {
-				//重组数据
-				this.list = this.orgData(this.initlist, 0);
-				//存在原始值，读区值
-				this.handlerRecover();
-				return;
-			}
-			let { currentIndex } = this;
-			switch (currentIndex) {
-				case 0:
-					this.list = this.convert(this.initlist);
-					this.showList = this.list;
-					break;
-				case 1:
-					let opts = this.current[0];
-					let city = this.initlist;
-					this.showList = city;
-					if (JSON.stringify(this.list) !== "{}") {
-						this.list[String(opts[this.clue]).trim()][
-							this.child
-						] = this.convert(city);
-					}
-					break;
-				case 2:
-					let _city = Object.assign({}, this.current[1]);
-					let county = this.initlist;
-					const province = this.current[0][this.clue];
-					this.showList = county;
-					if (
-						JSON.stringify(this.list) !== "{}" &&
-						this.list[province][this.child]
-					) {
-						this.list[province][this.child][_city[this.clue]][
-							this.child
-						] = this.convert(county);
-					}
-			}
+		pid:{  //使用重组数据方法时，制定关联字段key
+			type:String,
+			default:'parent_id'
 		}
-	},
-	methods: {
-		closeDropdown() {
-			this.showPanel = false;
-		},
-		toggleDropdown() {
-			this.showPanel = !this.showPanel;
-			//有默认值，清除默认值后展示面板，加载一级目录数据
+    },
+    data() {
+        return {
+            showPanel: false,
+            list: {},
+            showList: [],
+            currentIndex: 0, // 1,2,3
+            current: [
+                {
+                    name: "请选择"
+                }
+            ],
+            finish: ""
+        };
+    },
+    async created() {
+      	if(this.mode === 0) this.list = origidata;
+        if (Array.isArray(this.initShowList) && this.initShowList.length > 0) {
+            this.current = [].concat(this.initShowList);
+            this.finish = this.current
+                        .map(item => (item[this.name] ? item[this.name] : ""))
+                        .join(" ");
+        }
+        var ln = this.current.length;
+        if(this.mode === 0){
+       	 	this.showCurrent({},ln-1);
+        	return;
+        } 
+        this.showCurrent({},ln-1);
+    },
+    watch:{
+		finish() {
+			this.$emit("input", this.getValues());
+		}
+    },
+    methods: {
+        closeDropdown() {
+            this.showPanel = false;
+        },
+        toggleDropdown() {
+            this.showPanel = !this.showPanel;
+            //有默认值，清除默认值后展示面板，加载一级目录数据
 			if (
 				this.showPanel &&
 				JSON.stringify(this.list) === "{}" &&
 				this.currentIndex === 0
 			) {
-				this.getProvince();
+				this.showCurrent({},0)
 			}
-		},
-		handlerRecover() {
-			const ctl = this.current.length;
-			switch (ctl) {
-				case 2:
-					this.getCity(this.current[0]);
-					this.currentIndex = 1;
-					break;
-				case 3:
-					this.getCounty(this.current[1]);
-					this.currentIndex = 2;
-					break;
-			}
-			return;
-		},
-		//重组数据，
-		orgData(data, parent_id) {
+        },
+        //一次性加载数据时，重组数据，
+		orgData(data=[], parent_id) {
 			var tree = {};
 			var temp;
 			for (var i = 0; i < data.length; i++) {
@@ -243,153 +140,170 @@ export default {
 			}
 			return tree;
 		},
-		showCurrent(item, index) {
-			this.currentIndex = index;
-			switch (index) {
-				case 0: {
-					this.getProvince();
-					break;
-				}
-				case 1: {
-					this.getCity(this.current[0]);
-					break;
-				}
-				case 2: {
-					this.getCounty(this.current[1]);
-					break;
-				}
-				default: {
-					console.error("传入参数错误");
-				}
-			}
-		},
+        getValues(){
+            let area = [].concat(
+                    this.current.map(item => {
+                        return item[this.clue] && {
+                            [this.clue]: item[this.clue],
+                            [this.name]: item[this.name]
+                        };
+                    })
+                );
+            area = this.finish =="" ? [] : area;
+            return area;
+        },
+        async loadData(opts){
+        	this.showList = [];
+        	const promiseResult = await this.data(opts);
+        	//一次性加载所有数据，并且使用默认的重组数据方法
+        	if(this.mode === 1 && orgdata){
+        		return this.orgData(promiseResult);
+        	}
+        	return promiseResult;
+        },
+        async showCurrent(item, index) {
+            this.currentIndex = index;
+            switch (index) {
+                case 0: {
+                    this.showList = await this.getProvince();
+                    break;
+                }
+                case 1: {
+                    this.showList = await this.getCity(this.current[0]);
+                    break;
+                }
+                case 2: {
+                    this.showList = await this.getCounty(this.current[1]);
+                    break;
+                }
+                default: {
+                    console.error("传入参数错误");
+                }
+            }
+        },
+        async getNext(opts) {
+            const index = this.currentIndex;
+            switch (index) {
+                case 0: {
+                    const city = await this.getCity(opts);
+                    const current = [].concat(this.current);
+                    current[0] = Object.assign(opts);
+                    current[1] = Object.assign({
+                        name: "请选择"
+                    });
 
-		getNext(opts) {
-			const index = this.currentIndex;
-			switch (index) {
-				case 0: {
-					this.getCity(opts);
-					const current = [].concat(this.current);
-					current[0] = Object.assign(opts);
-					current[1] = Object.assign({
-						name: "请选择"
-					});
+                    if (opts.code !== this.current[0].code) {
+                        this.current = current.slice(0, 2);
+                    } else {
+                        this.current = current;
+                    }
 
-					if (opts[this.clue] !== this.current[0][this.clue]) {
-						this.current = current.slice(0, 2);
-					} else {
-						this.current = current;
-					}
-					this.currentIndex = 1;
-					break;
-				}
-				case 1: {
-					this.getCounty(opts);
-					this.current[1] = Object.assign({}, opts);
-					this.current[2] = Object.assign({
-						name: "请选择"
-					});
-					this.current = [].concat(this.current);
-					this.currentIndex = 2;
-					break;
-				}
-				case 2: {
-					this.current[2] = Object.assign({}, opts);
-					this.current = [].concat(this.current);
-					this.finish = [].concat(
-						this.current.map(item => {
-							return {
-								[this.clue]: item[this.clue],
-								[this.name]: item[this.name]
-							};
-						})
-					);
-					this.showPanel = false;
-					//用户捕获事件
-					this.$emit("onchange", this.finish);
-					break;
-				}
-				default: {
-					console.error("传入参数错误");
-				}
-			}
-		},
+                    this.showList = city;
+                    this.currentIndex = 1;
+                    break;
+                }
+                case 1: {
+                    const county = await this.getCounty(opts);
+                    this.current[1] = Object.assign({}, opts);
+                    this.current[2] = Object.assign({
+                        name: "请选择"
+                    });
+                    this.current = [].concat(this.current);
+                    this.showList = county;
+                    this.currentIndex = 2;
+                    break;
+                }
+                case 2: {
+                    this.current[2] = Object.assign({}, opts);
+                    this.current = [].concat(this.current);
+                    this.$emit("change", this.current);
+                    this.finish = this.current
+                        .map(item => (item[this.name] ? item[this.name] : ""))
+                        .join(" ");
+                    this.showPanel = false;
+                    this.$emit("onchange", this.getValues());
+                    break;
+                }
+                default: {
+                    console.error("传入参数错误");
+                }
+            }
+        },
 
-		convert(list) {
-			const data = {};
-			if (Array.isArray(list)) {
-				list.forEach(item => {
-					data[String(item[this.clue]).trim()] = item;
-				});
-			}
-			return data;
-		},
-		reconvert(obj) {
-			const data = [];
-			for (const i in obj) {
-				data.push(obj[i]);
-			}
-			return data;
-		},
-		getAllData() {
-			this.getAddress(opts);
-		},
-		getProvince() {
-			if (this.list && JSON.stringify(this.list) != "{}") {
-				this.showList = this.list;
-				return;
-			}
-			let opts = { level: 1 };
-			this.getAddress(opts);
-		},
-		getCity(opts) {
-			if (
-				this.list &&
-				JSON.stringify(this.list) !== "{}" &&
-				this.list[String(opts[this.clue]).trim()][this.child]
-			) {
-				const data = this.list[String(opts[this.clue]).trim()][
-					this.child
-				];
-				this.showList = data;
-				return;
-			}
-			let _opts = { level: 2, level_code: opts[this.clue] };
-			this.getAddress(_opts);
-		},
+        convert(list) {
+            const data = {};
+            if (Array.isArray(list)) {
+                list.forEach(item => {
+                    data[String(item.code).trim()] = item;
+                });
+            }
+            return data;
+        },
+        reconvert(obj) {
+            const data = [];
+            for (const i in obj) {
+                data.push(obj[i]);
+            }
+            return data;
+        },
 
-		getCounty(opts) {
-			const city = Object.assign({}, opts);
-			const province = this.current[0][this.clue];
-			if (
-				this.list &&
-				JSON.stringify(this.list) !== "{}" &&
-				this.list[province][this.child] &&
-				this.list[province][this.child][city[this.clue]] &&
-				this.list[province][this.child][city[this.clue]][this.child]
-			) {
-				const data = this.list[province][this.child][city[this.clue]][
-					this.child
-				];
-				this.showList = data;
-				return this.reconvert(data);
-			}
+        async getProvince() {
+            if (this.list && JSON.stringify(this.list) !=="{}") {
+                return this.list;
+            }
+            const province = await this.loadData({ level: 1 });
+            this.list = this.convert(province);
+            return this.list;
+        },
 
-			let _opts = { level: 3, level_code: city[this.clue] };
-			this.getAddress(_opts);
-		},
-		getAddress(opts) {
-			this.showList = [];
-			this.$emit("onload", opts);
-			return;
-		},
-		clear() {
-			this.finish = [];
+        async getCity(opts) {
+            if (this.list && JSON.stringify(this.list) !=="{}" && this.list[String(opts[this.clue]).trim()][this.child]) {
+                const data = this.list[String(opts[this.clue]).trim()][this.child];
+                return this.reconvert(data);
+            }
+            const city = await this.loadData({
+                level: 2,
+                level_code: opts[this.clue]
+            });
+            //兼容回显
+            if(JSON.stringify(this.list) !== "{}"){
+            	this.list[String(opts[this.clue]).trim()][this.child] = this.convert(city);
+            }
+            return city;
+        },
+        async getCounty(opts) {
+            const city = Object.assign({}, opts);
+            const province = this.current[0][this.clue];
+            if (
+                this.list &&
+                JSON.stringify(this.list) !=="{}" && 
+                this.list[province][this.child] &&
+                this.list[province][this.child][city[this.clue]] &&
+                this.list[province][this.child][city[this.clue]][this.child]
+            ) {
+                const data = this.list[province][this.child][city[this.clue]][this.child];
+                return this.reconvert(data);
+            }
+            const county = await this.loadData({
+                level: 3,
+                level_code: city[this.clue]
+            });
+            if(JSON.stringify(this.list) !== "{}" && JSON.stringify(this.list[province][this.child])){
+            	 this.list[province][this.child][city[this.clue]][this.child] = this.convert(
+                	county
+            	);
+            }
+            return county;
+        },
+        clear() {
+			this.finish = '';
 			this.current = [{ name: "请选择" }];
 			this.showList = this.list;
 			this.currentIndex = 0;
-			this.$emit("onchange", this.finish);
+			this.$emit("onchange", this.getValues());
 		}
-	}
+
+    }
 };
 </script>
+
